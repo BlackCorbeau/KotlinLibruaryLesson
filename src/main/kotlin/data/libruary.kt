c2cbd5d9dedce09bfa66607b9e13fa5bba722e95
Kirill <kremizov3@gmail.com>
2025-05-13 12:52:28 +0300

    feat(src/main/kotlin/data): add method Delete
    Added method delete to delete by: ip, title, author, date

--- src/main/kotlin/data/libruary.kt
+++ src/main/kotlin/data/libruary.kt
@@ -10,4 +10,33 @@ class Libruary(_had : MutableList<Book>? = null) {
     fun insert(book: Book) {
         had!!.add(book);
     }
+
+    fun delete(id: Int) {
+        had?.forEach { item ->
+            if (item.id == id ) {
+                had.remove(item)
+            }
+        }
+    }
+
+    fun delete( str : String) {
+        had?.forEach { item ->
+            if (item.title == str ) {
+                had.remove(item);
+                return;
+            } else if (item.author == str){
+                had.remove(item);
+                return;
+            }
+        }
+    }
+
+    fun delete( dat: LocalDate) {
+        had?.forEach{item ->
+            if (item.year == dat) {
+                had.remove(item);
+                return;
+            }
+        }
+    }
 }
